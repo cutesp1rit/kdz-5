6f666f8feab0d32b01485c356a1a9baa6aa5ac9d
Timofey Maksimov <tsmaksimov@edu.hse.ru>
2024-01-11 19:24:28 +0300

    Подготовить алгоритм к работе, ввести изначальные переменные, обозначить состояния

--- APP/Program.cs
+++ APP/Program.cs
@@ -1,10 +1,44 @@
 ﻿namespace APP
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public enum State
         {
-            Console.WriteLine("Hello, World!");
+            String,
+            Comment,
+            Program
+        }
+        public static void Main()
+        {
+            Console.WriteLine("Введите код программы:");
+            string? code = Console.ReadLine();
+            State state = State.Program;
+            int commentCount = 0;
+            int stringCount = 0;
+            foreach (var symbol in code ?? "")
+            {
+                switch (state)
+                {
+                    case State.Program when symbol == '/':
+                        commentCount++;
+                        state = State.Comment;
+                        break;
+                    case State.Program when symbol == '"':
+                        stringCount++;
+                        state = State.String;
+                        break;
+                    case State.Comment when symbol == '\n':
+                        state = State.Program;
+                        break;
+                    case State.String when symbol == '"':
+                        state = State.Program;
+                        break;
+                }
+            }
+
+            Console.WriteLine($"Количество строк: {stringCount}");
+            Console.WriteLine($"Количество комментариев: {commentCount}");
         }
     }
+
 }
--- LIBRARY/JsonParser.cs
+++ LIBRARY/JsonParser.cs
@@ -2,6 +2,59 @@
 {
     public static class JsonParser
     {
+        public enum State
+        {
+            Field,
+            ContentField,
+            ContentFieldForId, // это специальное состояние для id, так как оно идет без кавычек
+            ContentFileForMassiv // состояние для полей с данными из массивов
+        }
+        public static void ReadJson()
+        {
+            string[] massivOfFields = { "store_id", "store_name", "location", "employees", "products" };
+            Object[] myObjects = new object[][0];
+            
+            string allStrings = File.ReadAllText(fPath);
+            if (allStrings==null || allStrings.Length == 0) // если файл пустой или null выбрасываем исключение
+            {
+                // throw new ArgumentNullException();
+            }
 
+            int indexOfObject = 0; // индекс для заноса объектов в массив
+            int indexOfLetters = 0; // индекс по проходу всего файла
+            string nameField; string contentOfField;
+            
+            State state = State.Field;
+            while (indexOfLetters < allStrings.Length)
+            {
+                
+                char symbol = allStrings[indexOfLetters];
+                switch (state)
+                {
+                    case State.Field when symbol == '"':
+                        commentCount++;
+                        state = State.Comment;
+                        indexOfLetters++;
+                        break;
+                    case State.ContentField when symbol == '"':
+                        stringCount++;
+                        state = State.String;
+                        indexOfLetters++;
+                        break;
+                    case State.Comment when symbol == '\n':
+                        state = State.Program;
+                        indexOfLetters++;
+                        break;
+                    case State.String when symbol == '"':
+                        state = State.Program;
+                        indexOfLetters++;
+                        break;
+                    case State.Field when symbol == '}':
+                        indexOfObject++;
+                        indexOfLetters++;
+                        break;
+                }
+            }
+        }
     }
 }
